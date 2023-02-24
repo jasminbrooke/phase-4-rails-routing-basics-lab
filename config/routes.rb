be25286824a6573897f6e163a9a9963a234f3371
+ <+>
2023-02-23 20:59:42 -0500

    commit tree b3330294b5d3ff8de79321ffc982cfdb4fe8c086

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/students', to: 'students#index'
+  # resources :students, only: [:index] do
+  #   collection do
+  #     :grades
+  #   end
+  # end
+
+  get '/students/grades', to: 'students#grades'
+  
+  get '/students/highest-grade', to: 'students#highest_grade'
 end
